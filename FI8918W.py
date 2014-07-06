b948bdd8d92424e38f18f8c4b86125f59008cffc
nhuffman <nathanael.huffman@gmail.com>
2014-07-06 12:50:49 -0500

    Converted the authentication scheme from using cgi parameters to using http basic authentication. This appears to be the more general solution since the decoder_control.cgi script doesn't seem to accept the authentication parameters but still requires authentication to control the camera.  Using basic HTTP authentication seems to make this interface work.

--- FI8918W.py
+++ FI8918W.py
@@ -1,4 +1,8 @@
 import urllib2
+import re
+import base64
+import sys
+import os
 
 
 class fi8918w:
@@ -7,7 +11,7 @@ class fi8918w:
     """
 
     def __init__(self, url="", username="", password=""):
-        self.url = url
+        self.camera_url = url
         self.username = username
         self.password = password
         self.camera_id = ""
@@ -15,43 +19,69 @@ class fi8918w:
         self.alarm_status = ""
 
     # ---------- Private methods ----------
+    def _basic_auth(self, url):
+        """ This method takes the requested URL, check if basic authentication is required and sets up the HTTP GET with the
+        base64 authentication.  Used http://www.voidspace.org.uk/python/articles/authentication.shtml as a guide for doing this
+        :param url: url to be requested from IP camera
+        :return: returns the urllib2.urlopen return value
+        """
+        if not self.username or not self.password or not url:
+            # invalid request
+            return -1
+        # Make a request
+        req = urllib2.Request(url)
+        try:
+            handle = urllib2.urlopen(req)
+        except IOError, e:
+            # Here we want to fail if there's authentication required on this page and we'll continue
+            pass
+        else:
+            # This page is not protected by authentication
+            return handle
+        # Extract the scheme and realm in using reg-ex
+        authline = e.headers['www-authenticate']
+        authobj = re.compile(r'''(?:\s*www-authenticate\s*:)?\s*(\w*)\s+realm=['"]([^'"]+)['"]''', re.IGNORECASE)
+        matchobj = authobj.match(authline)
+        scheme = matchobj.group(1)
+        realm = matchobj.group(2)   # un-used
+        if scheme.lower() != 'basic':
+            print "Basic authentication failed for some reason"
+            sys.exit(1)
+        # Encode the basic authentication into a base64 string
+        base64string = base64.encodestring('%s:%s' % (self.username, self.password))[:-1]
+        authheader = "Basic %s" % base64string
+        # Add the authentication header
+        req.add_header("Authorization", authheader)
+        return urllib2.urlopen(req)
+
     def _query_camera(self, command, arg=None):
-        if self.username == "" or self.password == "":
+        """ This method sends requests/commands to the camera when the return does not need to be saved to a binary file.
+        :param command: cgi request/command to the camera
+        :return: returns the urllib2.urlopen return value
+        """
+        if not self.camera_url or not command:
             return -1
+        nurl = "http://" + self.camera_url + "/" + command
+        # Set up the basic authentication, and get the data
+        return self._basic_auth(nurl)
 
-        if self.url == "":
+    def _query_camera_binary(self, command, arg='picture'):
+        """
+        This method sends requests/commands to the camera when the return does need to be saved to a binary file.
+        Used for saving snapshots from the camera for example.
+        :param command: cgi request/command to the camera
+        :param arg: base pathname and file name without extension where the picture will be saved
+        :return: pathname and filename of the output file
+        """
+        if not self.camera_url or not command:
             return -1
-
-        nurl = "http://" + self.url + "/" + command
-        nurl += "?user=" + self.username + "&pwd=" + self.password
-        if arg is not None:
-            nurl = nurl + "&" + arg
-
-        r = urllib2.urlopen(nurl)
-        resp = r.readlines()
-        #resp = r.read()
-
-        return resp
-
-    def _query_camera_binary(self, command, arg='picture', ext='jpg'):
-        if self.username == "" or self.password == "":
-            return -1
-
-        if self.url == "":
-            return -1
-
-        nurl = "http://" + self.url + "/" + command
-        nurl += "?user=" + self.username + "&pwd=" + self.password
-        nurl += "&next_url=" + arg
-
-        fname = arg + '.' + ext
-
-        b = urllib2.urlopen(nurl)
-        fout = open(fname, 'wb')
-        fout.write(b.read())
-        fout.close()
-
-        return fname
+        nurl = "http://" + self.camera_url + "/" + command
+        # Binary commands pass the filename to the next_url parameter without  file extension
+        nurl += "?next_url=" + os.path.splitext(arg)[0]
+        b = self._basic_auth(nurl)
+        with open(arg, 'wb') as fout:
+            fout.write(b.read())
+        return arg
 
     # ---------- Public methods ----------
     def getStatus(self):
@@ -88,16 +118,24 @@ class fi8918w:
 
         resp = self._query_camera('set_alarm.cgi', arg)
 
-        if resp == -1:
-            return -1
-        else:
-            return 1
+        return -1 if resp == -1 else 1
 
     def getSnapshot(self, fname='picture'):
-        status = self._query_camera_binary('snapshot.cgi', fname)
+        return self._query_camera_binary('snapshot.cgi', fname)
 
-        return status
+    def ir_on(self):
+        """ This method sends command 95 to the camera which turns on the infrared emitters
+        :return: returns the url2lib response from the camera
+        """
+        resp = self._query_camera('decoder_control.cgi?command=95')
+        return resp
 
+    def ir_off(self):
+        """ This method sends command 94 to the camera which turns off the infrared emitters
+        :return: returns the url2lib response from the camera
+        """
+        resp = self._query_camera('decoder_control.cgi?command=94')
+        return resp
 
 if __name__ == "__main__":
     print("pyFoscam: import this module to use it")
