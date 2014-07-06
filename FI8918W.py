d1d8e19a7b30abee8c7923233d110252b2ece5e6
nhuffman <nathanael.huffman@gmail.com>
2014-07-06 12:36:02 -0500

    Misc style cleanup to match PEP8.  Left the public interface and class name alone for now even though they don't conform to PEP8

--- FI8918W.py
+++ FI8918W.py
@@ -1,146 +1,103 @@
-import urllib
 import urllib2
-import numpy as np
-import cv2
 
-"""
-This module facilitates the use of a Foscam FI8918W IP Camera. It may work
-with other Foscam (or any other) cameras that have the same API.
-"""
 
 class fi8918w:
+    """
+    This class can be used to send commands to a Foscam FI8918W IP camera.
+    """
 
-  """
-  This class can be used to send commands to a Foscam IP camera.
-  """
+    def __init__(self, url="", username="", password=""):
+        self.url = url
+        self.username = username
+        self.password = password
+        self.camera_id = ""
+        self.camera_name = ""
+        self.alarm_status = ""
 
-  def __init__(self, url="", userName="", passWord=""):
-    self.url = url
-    self.userName = userName
-    self.passWord = passWord
-    self.cameraId = ""
-    self.cameraName = ""
-    self.irStatus = ""
+    # ---------- Private methods ----------
+    def _query_camera(self, command, arg=None):
+        if self.username == "" or self.password == "":
+            return -1
 
-  # ---------- Private methods ----------
-  def __queryCamera(self, command, arg = None):
-    if self.userName == "" or self.passWord == "":
-      return -1
+        if self.url == "":
+            return -1
 
-    if self.url == "":
-      return -1;
+        nurl = "http://" + self.url + "/" + command
+        nurl += "?user=" + self.username + "&pwd=" + self.password
+        if arg is not None:
+            nurl = nurl + "&" + arg
 
-    nurl = "http://" + self.url + "/" + command
-    nurl += "?user=" + self.userName + "&pwd=" + self.passWord
-    if arg != None:
-      nurl = nurl + "&" + arg
+        r = urllib2.urlopen(nurl)
+        resp = r.readlines()
+        #resp = r.read()
 
-    r = urllib2.urlopen(nurl)
-    resp = r.readlines()
-    #resp = r.read()
- 
-    return resp
+        return resp
 
-  def __queryCameraBinaryToFile(self, command, arg = 'picture', ext = 'jpg'):
-    if self.userName == "" or self.passWord == "":
-      return -1
+    def _query_camera_binary(self, command, arg='picture', ext='jpg'):
+        if self.username == "" or self.password == "":
+            return -1
 
-    if self.url == "":
-      return -1
+        if self.url == "":
+            return -1
 
-    nurl = "http://" + self.url + "/" + command
-    nurl += "?user=" + self.userName + "&pwd=" + self.passWord
-    nurl += "&next_url=" + arg
+        nurl = "http://" + self.url + "/" + command
+        nurl += "?user=" + self.username + "&pwd=" + self.password
+        nurl += "&next_url=" + arg
 
-    fname = arg + '.' + ext
+        fname = arg + '.' + ext
 
-    b = urllib2.urlopen(nurl)
-    fout = open(fname, 'wb')
-    fout.write(b.read())
-    fout.close()
+        b = urllib2.urlopen(nurl)
+        fout = open(fname, 'wb')
+        fout.write(b.read())
+        fout.close()
 
-    return fname
+        return fname
 
-  def __queryCameraBinaryToImage(self, command):
-    if self.userName == "" or self.passWord == "":
-      return -1
+    # ---------- Public methods ----------
+    def getStatus(self):
+        resp = self._query_camera('get_status.cgi')
 
-    if self.url == "":
-      return -1
+        if resp == -1:
+            status = -1
+        else:
+            status = {}
+            for line in resp:
+                line = line[4:]  # strip "var "
+                line = line.rstrip(';\n')
+                parts = line.split('=')
+                if len(parts) > 1:
+                    if parts[1].rfind("'") == -1:
+                        status[parts[0]] = parts[1]
+                    else:
+                        status[parts[0]] = parts[1].replace("'", "")
 
-    nurl = "http://" + self.url + "/" + command
-    nurl += "?user=" + self.userName + "&pwd=" + self.passWord
+                if parts[0] == 'alarm_status':
+                    self.alarm_status = status['alarm_status']
+                elif parts[0] == 'id':
+                    self.camera_id = status['id']
+                elif parts[0] == 'alias':
+                    self.camera_name = status['alias']
 
-    b = urllib2.urlopen(nurl)
-    img_str = b.read()
-    nparr = np.fromstring(img_str, np.uint8)
-    img = cv2.imdecode(nparr, cv2.CV_LOAD_IMAGE_COLOR)
+        return status
 
-    return img
+    def setMotionAlarm(self, alarmOn):
+        if alarmOn:
+            arg = "motion_armed=1"
+        else:
+            arg = "motion_armed=0"
 
-  # ---------- Public methods ----------
-  def getStatus(self):
-    resp = self.__queryCamera('get_params.cgi')
+        resp = self._query_camera('set_alarm.cgi', arg)
 
-    if resp == -1:
-      status = -1
-    else:
-      status = {}
-      for line in resp:
-        line = line[4:] # strip "var "
-        line = line.rstrip(';\n')
-        parts = line.split('=')
-        if len(parts) > 1:
-          if parts[1].rfind("'") == -1:
-            status[parts[0]] = parts[1]
-          else:
-            status[parts[0]] = parts[1].replace("'", "")
+        if resp == -1:
+            return -1
+        else:
+            return 1
 
-        if parts[0] == 'alarm_motion_armed':
-          self.irStatus = status['alarm_motion_armed']
-        elif parts[0] == 'id':
-          self.cameraId = status['id']
-        elif parts[0] == 'alias':
-          self.cameraName = status['alias']
+    def getSnapshot(self, fname='picture'):
+        status = self._query_camera_binary('snapshot.cgi', fname)
 
-    return status
-
-  def setMotionAlarm(self, alarmOn):
-    if alarmOn == True:
-      arg = "motion_armed=1"
-    else:
-      arg = "motion_armed=0"
-
-    resp = self.__queryCamera('set_alarm.cgi', arg)
-
-    if resp == -1:
-      return -1
-    else:
-      return 1
-
-  def setIr(self, irOn):
-    if irOn == True:
-      arg = "command=95"
-    else:
-      arg = "command=94"
-
-    resp = self.__queryCamera('decoder_control.cgi', arg)
-
-    if resp == -1:
-      return -1
-    else:
-      return 1
-
-  def getSnapshotToFile(self, fname='picture'):
-    status = self.__queryCameraBinaryToFile('snapshot.cgi', fname)
-
-    return status
-
-  def getSnapshotToImage(self):
-    status = self.__queryCameraBinaryToImage('snapshot.cgi')
-
-    return status
+        return status
 
 
 if __name__ == "__main__":
-  print("pyFoscam: import this module to use it")
+    print("pyFoscam: import this module to use it")
